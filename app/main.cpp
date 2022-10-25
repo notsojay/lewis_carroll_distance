1426b5bf5f96bdec74f6b5c39378a0bafe4f9238
Jiahao Liang <jiahal41@uci.edu>
2022-10-25 14:42:21 -0700

    more positive

--- app/main.cpp
+++ app/main.cpp
@@ -7,16 +7,6 @@
 
 int main()
 {
-
-
-    WordSet words(11);
-    std::ifstream in("words.txt");
-    loadWordsIntoTable(words, in);
-    std::cout <<words.getCapacity() << "\n" << words.getCount() << '\n';
-    
-    std::vector< std::string > r  = convert("ant", "eat", words);
-    for(auto &i : r)
-        std::cout << i << "  ";
     return 0;
 }
 
